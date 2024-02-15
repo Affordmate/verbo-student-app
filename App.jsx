f5dbd7221c24956665d7d482f128d2bfe3c1e300
abhiramitp <abhirami.tp@affordmate.com>
2024-02-15 14:35:54 +0530

    Package

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import {ProfileCard} from 'verbo-component';
+
+function App() {
+  return (
+    <View>
+      <ProfileCard />
+      {/* <Text>Hii</Text> */}
+    </View>
+  );
+}
+
+export default App;
