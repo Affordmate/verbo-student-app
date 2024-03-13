75cc2f7875e42999d1968bdd014acecd53da06a7
abhiramitp <abhirami.tp@affordmate.com>
2024-03-13 12:35:45 +0530

    all changes

--- App.jsx
+++ App.jsx
@@ -1,30 +1,51 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+<>
+  {item.mode !== 990 && item.mode !== 991 && ( // Render only if mode is not e-hailing or ambulance
+    <>
+      <Label label={t("form.departure")} />
+      <DropdownList
+        data={locations} // Assuming you have the list of locations for departure
+        search={false}
+        setSelected={item.destination}
+        bg={true}
+        onSelect={(item) => {
+          var clonedPivotColumns = transportation.slice();
+          clonedPivotColumns[index].destination = item.key;
+          setTransportation(clonedPivotColumns);
+        }}
+        style={{
+          backgroundColor: "#fff",
+          borderWidth: 1,
+          borderColor: "#000",
+          borderRadius: 10,
+        }}
+      />
+    </>
+  )}
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import {ProfileCard} from 'verbo-component';
+  <Break style={{ paddingTop: 20 }} />
 
-function App() {
-  return (
-    <View>
-      <ProfileCard />
-      {/* <Text>Hii</Text> */}
-    </View>
-  );
-}
+  {item.mode !== 990 && item.mode !== 991 && (
+    <>
+      <Label label={t("form.arrival")} />
+      <DropdownList
+        data={locations} // Assuming you have the list of locations for arrival
+        search={false}
+        setSelected={item.arrival}
+        bg={true}
+        onSelect={(item) => {
+          var clonedPivotColumns = transportation.slice();
+          clonedPivotColumns[index].arrival = item.key;
+          setTransportation(clonedPivotColumns);
+        }}
+        style={{
+          backgroundColor: "#fff",
+          borderWidth: 1,
+          borderColor: "#000",
+          borderRadius: 10,
+        }}
+      />
+    </>
+  )}
 
-export default App;
+  <Break style={{ paddingTop: 20 }} />
+</>
